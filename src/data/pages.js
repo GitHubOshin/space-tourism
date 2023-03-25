63a5a87913eb2622590fa8fe6b71738a51896ef1
OSHIN <izkp44@gmail.com>
2023-03-26 04:44:46 +0700

    Create pages.js

---
+++ src/data/pages.js
@@ -0,0 +1,8 @@
+const pages = [
+  { number: '00', pageName: 'HOME', href: '' },
+  { number: '01', pageName: 'DESTINATION', href: '' },
+  { number: '02', pageName: 'CREW', href: '' },
+  { number: '03', pageName: 'TECHNOLOGY', href: '' }
+]
+
+export default pages
