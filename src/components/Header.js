002b4f13bdd42713a857a9f02424d98eed6d05be
OSHIN <izkp44@gmail.com>
2023-03-28 16:55:40 +0700

    Edit margin / padding

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
-import MenuBar from './MenuBar'
+import NavBar from './NavBar'
 
 function Header() {
   return (
-    <header className="w-full flex justify-between items-center pl-[55px] mt-10 tablet:gap-[231px]">
+    <header className="w-full flex justify-between items-center tablet:pl-[55px] desktop:mt-10 tablet:gap-[231px]">
       <img alt="Logo" src="images/shared/logo.svg" className="w-12 h-12" />
-      <MenuBar />
+      <NavBar />
     </header>
   )
 }
