fa04edea090d27d2e9011742e775adfb2d2e0b4b
OSHIN <izkp44@gmail.com>
2023-03-28 17:23:19 +0700

    Update SectionHome.js

--- src/sections/SectionHome.js
+++ src/sections/SectionHome.js
@@ -10,18 +10,18 @@ function SectionHome() {
       />
       <img
         alt="background"
-        src="images/home/background-home-tablet.jpg"
+        src="images/home/bg-home-tablet.png"
         className="w-full h-full mobile:hidden tablet:block desktop:hidden  min-w-[375px]"
       />
       <img
         alt="background"
-        src="images/home/background-home-mobile.jpg"
+        src="images/home/bg-home-mobile.png"
         className="w-full h-full tablet:hidden min-w-[375px]"
       />
       <div className="absolute top-0 w-full h-full flex flex-col items-center mobile:gap-y-[48px] tablet:gap-y-[106px] desktop:gap-y-[251px] mobile:pt-6 tablet:pt-0 mobile:px-6 tablet:px-0">
         <Header />
         <div className="grid desktop:grid-cols-2 w-full max-w-[1110px] mobile:justify-center mobile:gap-y-[81px] tablet:gap-y-[156px] desktop:gap-y-0">
-          <div className="w-full max-w-[450px] text-white mobile:text-center desktop:text-left">
+          <div className="w-full max-w-[450px] text-white mobile:text-center desktop:text-left flex flex-col gap-y-4">
             <span className="text-babyBlue mobile:text-[16px] tablet:text-[20px] desktop:text-[28px] mobile:tracking-[2.7px] tablet:tracking-[3.38px] desktop:tracking-[4.72px] font-barlowCondensed">
               SO, YOU WANT TO TRAVEL TO
             </span>
