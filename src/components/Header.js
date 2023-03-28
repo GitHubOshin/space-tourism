49035f7dee4fe4fe24059dd9fd537f475d8fc811
OSHIN <izkp44@gmail.com>
2023-03-28 17:20:44 +0700

    Edit size of logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,11 @@ import NavBar from './NavBar'
 function Header() {
   return (
     <header className="w-full flex justify-between items-center tablet:pl-[55px] desktop:mt-10 tablet:gap-[231px]">
-      <img alt="Logo" src="images/shared/logo.svg" className="w-12 h-12" />
+      <img
+        alt="Logo"
+        src="images/shared/logo.svg"
+        className="mobile:w-10 mobile:h-10 tablet:w-12 tablet:h-12"
+      />
       <NavBar />
     </header>
   )
