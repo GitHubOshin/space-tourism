baaa85d2a5d323379e7369290db50829649dda5c
OSHIN <izkp44@gmail.com>
2023-03-30 18:50:47 +0700

    Create Buttons.js

---
+++ src/components/Buttons.js
@@ -0,0 +1,7 @@
+export function DestinationMenu(props) {
+  return (
+    <button className="text-babyBlue hover:text-white font-barlowCondensed text-[16px] tracking-[2.7px] hover:border-b-[3px]">
+      {props.name}
+    </button>
+  )
+}
