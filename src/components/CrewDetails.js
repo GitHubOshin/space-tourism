c232ddd499d9856a26ac9ef094e37fa4976050d0
OSHIN <izkp44@gmail.com>
2023-03-31 23:57:46 +0700

    Create CrewDetails.js

---
+++ src/components/CrewDetails.js
@@ -0,0 +1,11 @@
+import TitlePage from './TitlePage'
+
+function CrewDetails() {
+  return (
+    <div className="">
+      <TitlePage number="02" title="MEET YOUR CREW" />
+    </div>
+  )
+}
+
+export default CrewDetails
