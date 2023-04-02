e4b73b4b0bb17f32efa6f2ddabc741994f822fbb
OSHIN <izkp44@gmail.com>
2023-04-02 19:16:53 +0700

    Import BgAndHeader

--- src/pages/CrewPage.js
+++ src/pages/CrewPage.js
@@ -1,29 +1,15 @@
 import CrewDetails from '../components/CrewDetails'
-import Header from '../components/Header'
+import BgAndHeader from '../components/BgAndHeader'
 
 function CrewPage() {
   return (
-    <section>
-      <img
-        alt="background"
-        src="images/crew/background-crew-desktop.jpg"
-        className="w-full h-full mobile:hidden desktop:block"
-      />
-      <img
-        alt="background"
-        src="images/crew/background-crew-tablet.jpg"
-        className="w-full h-full mobile:hidden tablet:block desktop:hidden min-w-[375px]"
-      />
-      <img
-        alt="background"
-        src="images/crew/background-crew-mobile.jpg"
-        className="w-full h-full tablet:hidden min-w-[375px]"
-      />
-      <div className="absolute top-0 w-full h-full">
-        <Header />
-        <CrewDetails />
-      </div>
-    </section>
+    <BgAndHeader
+      bgDesktop="images/crew/background-crew-desktop.jpg"
+      bgTablate="images/crew/background-crew-tablet.jpg"
+      bgMobile="images/crew/background-crew-mobile.jpg"
+    >
+      <CrewDetails />
+    </BgAndHeader>
   )
 }
 
