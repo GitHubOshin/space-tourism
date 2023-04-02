bdf657e151e536eb448ecd7dc905d363196f19a6
OSHIN <izkp44@gmail.com>
2023-04-02 13:19:36 +0700

    Update CrewDetails.js

--- src/components/CrewDetails.js
+++ src/components/CrewDetails.js
@@ -2,8 +2,12 @@ import TitlePage from './TitlePage'
 
 function CrewDetails() {
   return (
-    <div className="">
+    <div className="bg-cyan-500 desktop:w-[1110px]">
       <TitlePage number="02" title="MEET YOUR CREW" />
+      <div className="bg-teal-900 flex mobile:flex-col desktop:flex-row">
+        <div className="w-full max-w-[488.5px] desktop:h-[406px] bg-purple-300"></div>
+        <img alt="" src="images/crew/image-douglas-hurley.png" />
+      </div>
     </div>
   )
 }
