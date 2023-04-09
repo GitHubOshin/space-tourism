5bb107cb44ac4e12b55781bcbd36425e82626b56
OSHIN <izkp44@gmail.com>
2023-04-09 23:53:33 +0700

    Fetch data from JSON

--- src/components/contents/DestinationContent.js
+++ src/components/contents/DestinationContent.js
@@ -1,16 +1,35 @@
+import axios from 'axios'
 import { DestinationMenu } from '../Buttons'
 import TitlePage from '../TitlePage'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 function DestinationContent(props) {
   const { image, name, description, distance, travel } = props
+  const [destinations, setDestinations] = useState([])
+
+  useEffect(() => {
+    fetchDestinations()
+  }, [])
+
+  async function fetchDestinations() {
+    try {
+      const { data } = await axios.get('data.json')
+      setDestinations(data.destinations)
+    } catch (error) {
+      console.error('❌ Error:', error.response)
+    }
+  }
+
+  const [moon /*, mars, europa, titan*/] = destinations
 
   return (
     <div className="desktop:w-[1110px] h-[576px] flex flex-col justify-between">
       <TitlePage number="01" title="PICK YOUR DESTINATION" />
       <div className="self-end w-full desktop:w-[1047px] h-[478px] grid desktop:grid-cols-2 desktop:gap-[166px]">
         <img
-          alt={name}
-          src={image}
+          alt={moon?.name || ''}
+          src={moon?.images?.png || ''}
           className="self-end mobile:w-[170px] mobile:h-[170px] tablet:w-[300px] tablet:h-[300px] desktop:w-[445px] desktop:h-[445px]"
         />
         <div className="max-w-[445px]">
@@ -21,10 +40,10 @@ function DestinationContent(props) {
             <DestinationMenu name="TITAN" />
           </div>
           <h1 className="text-white font-bellefair mobile:text-[56px] tablet:text-[80px] desktop:text-[100px] mt-[10px] mb-[10px]">
-            {name}
+            {moon.name}
           </h1>
           <p className="text-babyBlue mobile:text-[15px] tablet:text-[16px] desktop:text-[18px] font-barlow mobile:leading-[25px] tablet:leading-[28px] desktop:leading-[32px]">
-            {description}
+            {moon?.description || ''}
           </p>
           <hr className="border-[#383B4B] mt-[54px] mb-[28px]" />
           <div className="grid grid-cols-2">
@@ -33,14 +52,16 @@ function DestinationContent(props) {
                 AVG. DISTANCE
               </h3>
               <p className="text-white text-[28px] font-bellefair">
-                {distance}
+                {moon?.distance || ''}
               </p>
             </div>
             <div className="">
               <h3 className="text-babyBlue text-[14px] tracking-[2.36px] font-barlowCondensed">
                 AVG. DISTANCE
               </h3>
-              <p className="text-white text-[28px] font-bellefair">{travel}</p>
+              <p className="text-white text-[28px] font-bellefair">
+                {moon.travel}
+              </p>
             </div>
           </div>
         </div>
