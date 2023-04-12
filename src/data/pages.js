e6060056dcef88ce71b4796a26858cfbc7915491
OSHIN <izkp44@gmail.com>
2023-04-12 23:18:50 +0700

    Edit href

--- src/data/pages.js
+++ src/data/pages.js
@@ -1,8 +1,8 @@
 const pages = [
-  { number: '00', pageName: 'HOME', href: '' },
-  { number: '01', pageName: 'DESTINATION', href: '' },
-  { number: '02', pageName: 'CREW', href: '' },
-  { number: '03', pageName: 'TECHNOLOGY', href: '' }
+  { number: '00', pageName: 'HOME', href: '/' },
+  { number: '01', pageName: 'DESTINATION', href: 'destination' },
+  { number: '02', pageName: 'CREW', href: 'crew' },
+  { number: '03', pageName: 'TECHNOLOGY', href: 'technology' }
 ]
 
 export default pages
