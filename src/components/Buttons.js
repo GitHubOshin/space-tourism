a1f37a4028b99534b747532b851b3663d40ba231
OSHIN <izkp44@gmail.com>
2023-04-15 23:24:37 +0700

    Create TechnologyNumberButton

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -5,3 +5,11 @@ export function DestinationMenu(props) {
     </button>
   )
 }
+
+export function TechnologyNumberButton(props) {
+  return (
+    <button className="mobile:w-10 mobile:h-10 tablet:w-[60px] tablet:h-[60px] desktop:w-[80px] desktop:h-[80px] text-white hover:text-black desktop:text-[32px] font-bellefair rounded-full hover:bg-white border border-[#4c4d56]">
+      {props.number}
+    </button>
+  )
+}
