bac5c11461f685182c3d1b5c891c911c0a55eb5c
OSHIN <izkp44@gmail.com>
2023-04-16 19:50:28 +0700

    Add /

--- src/data/pages.js
+++ src/data/pages.js
@@ -1,8 +1,8 @@
 const pages = [
-  { number: '00', pageName: 'HOME', href: '/' },
-  { number: '01', pageName: 'DESTINATION', href: 'destination' },
-  { number: '02', pageName: 'CREW', href: 'crew' },
-  { number: '03', pageName: 'TECHNOLOGY', href: 'technology' }
+  { number: '00', name: 'HOME', url: '/' },
+  { number: '01', name: 'DESTINATION', url: '/destinations' },
+  { number: '02', name: 'CREW', url: '/crew' },
+  { number: '03', name: 'TECHNOLOGY', url: '/technology' }
 ]
 
 export default pages
