a41c68c72215800c652c10f5f89cf63e7ef53d63
OSHIN <izkp44@gmail.com>
2023-05-24 20:08:53 +0700

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,14 +6,16 @@ function NavBar() {
     <nav className="relative w-full max-w-[830px] h-[96px] backdrop-blur bg-[hsl(0,0%,100%,0.1)] tablet:flex mobile:hidden justify-center items-center">
       <hr className="absolute right-[800px] border-[#979797] opacity-40 w-[437px] desktop:block tablet:hidden" />
       <div className="w-full h-full max-w-[542px] flex justify-between desktop:px-0 tablet:px-12">
-        {pages.map((page) => {
+        {pages.map(({ number, name, url }) => {
           return (
-            <button className="flex items-center gap-[14px] desktop:text-base tablet:text-sm h-full text-white font-barlowCondensed tracking-[2.7px] hover:border-b-[3px]">
-              <b className="tablet:hidden desktop:block font-bold">
-                {page.number}
-              </b>
-              <Link to={page.href}>{page.pageName}</Link>
-            </button>
+            <Link
+              key={[number, name].join('-')}
+              to={url}
+              className="flex items-center gap-[14px] desktop:text-base tablet:text-sm h-full text-white font-barlowCondensed tracking-[2.7px] hover:border-b-[3px]"
+            >
+              <b className="tablet:hidden desktop:block font-bold">{number}</b>
+              <span>{name}</span>
+            </Link>
           )
         })}
       </div>
