3233c8d72ca7fea8dfe7a11db403c3461700ef65
OSHIN <izkp44@gmail.com>
2023-06-11 20:46:56 +0700

    Update Header.js: add /

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
     <header className="w-full flex justify-between items-center tablet:pl-[55px] desktop:mt-10 tablet:gap-[231px]">
       <img
         alt="Logo"
-        src="images/shared/logo.svg"
+        src="/images/shared/logo.svg"
         className="mobile:w-10 mobile:h-10 tablet:w-[48px] tablet:h-[48px]"
       />
       <NavBar />
