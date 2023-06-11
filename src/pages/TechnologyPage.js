7995d2af884dc0f9c739e7a1e72ff819430e5177
OSHIN <izkp44@gmail.com>
2023-06-11 20:45:56 +0700

    Update TechnologyPage.js: add /

--- src/pages/TechnologyPage.js
+++ src/pages/TechnologyPage.js
@@ -4,9 +4,9 @@ import TechnologyContent from '../components/contents/TechnologyContent'
 function TechnologyPage() {
   return (
     <BgAndHeader
-      bgDesktop="images/technology/background-technology-desktop.jpg"
-      bgTablate="images/technology/background-technology-tablet.jpg"
-      bgMobile="images/technology/background-technology-mobile.jpg"
+      bgDesktop="/images/technology/background-technology-desktop.jpg"
+      bgTablate="/images/technology/background-technology-tablet.jpg"
+      bgMobile="/images/technology/background-technology-mobile.jpg"
     >
       <TechnologyContent />
     </BgAndHeader>
