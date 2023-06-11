a0aab8db81b0d898a25d86679f1f37b2163bfd8d
OSHIN <izkp44@gmail.com>
2023-06-11 20:44:32 +0700

    Update CrewPage.js: add /

--- src/pages/CrewPage.js
+++ src/pages/CrewPage.js
@@ -4,9 +4,9 @@ import BgAndHeader from '../components/BgAndHeader'
 function CrewPage() {
   return (
     <BgAndHeader
-      bgDesktop="images/crew/background-crew-desktop.jpg"
-      bgTablate="images/crew/background-crew-tablet.jpg"
-      bgMobile="images/crew/background-crew-mobile.jpg"
+      bgDesktop="/images/crew/background-crew-desktop.jpg"
+      bgTablate="/images/crew/background-crew-tablet.jpg"
+      bgMobile="/images/crew/background-crew-mobile.jpg"
     >
       <CrewContent />
     </BgAndHeader>
