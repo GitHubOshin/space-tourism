4296300e8346b24cd29bf197114cd740cd3da714
OSHIN <izkp44@gmail.com>
2023-06-11 20:48:00 +0700

    Update DestinationContent.js

--- src/components/contents/DestinationContent.js
+++ src/components/contents/DestinationContent.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import TitlePage from '../TitlePage'
 import { useState } from 'react'
 import { useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 const destinationLinks = [
   {
@@ -26,28 +26,61 @@ const destinationLinks = [
 function DestinationContent() {
   const [destinations, setDestinations] = useState([])
 
+  const location = useLocation()
+
   useEffect(() => {
     fetchDestinations()
   }, [])
 
   async function fetchDestinations() {
     try {
-      const { data } = await axios.get('data.json')
+      const { data } = await axios.get('/data.json')
       setDestinations(data.destinations)
     } catch (error) {
       console.error('❌ Error:', error.response)
     }
   }
 
-  const [moon /*, mars, europa, titan*/] = destinations
+  function mapDestByDestID(destinations = []) {
+    const dictionary = {}
+
+    destinations.forEach((dest) => {
+      const destName = (dest?.name || '').toLowerCase()
+      dictionary[destName] = dest
+    })
+
+    return dictionary
+  }
+
+  const dictDest = mapDestByDestID(destinations)
+
+  function findDestinationByName(destName) {
+    // return destinations.find((dest) => {
+    //   return (dest?.name || '').toLowerCase() === destName
+    // })
+
+    return dictDest[destName]
+  }
+
+  function getLastPath(url = '') {
+    const subPaths = url.split('/')
+    const lastPathStr = subPaths[subPaths.length - 1]
+    return lastPathStr
+  }
+  const destName = getLastPath(location.pathname)
+
+  console.log('lastPath:', destName)
+
+  const foundDestination = findDestinationByName(destName)
+  console.log('', foundDestination)
 
   return (
     <div className="desktop:w-[1110px] h-[576px] mobile:text-center desktop:text-start flex flex-col mobile:gap-[31px] tablet:gap-[59px]">
       <TitlePage number="01" title="PICK YOUR DESTINATION" />
       <div className="self-end w-full desktop:w-[1047px] h-[478px] grid desktop:grid-cols-2 desktop:gap-[166px]">
         <img
-          alt={moon?.name || ''}
-          src={moon?.images?.png || ''}
+          alt={foundDestination?.name || ''}
+          src={foundDestination?.images?.png || ''}
           className="desktop:self-end m-auto mobile:w-[170px] mobile:h-[170px] tablet:w-[300px] tablet:h-[300px] desktop:w-[445px] desktop:h-[445px]"
         />
         <div className="max-w-[445px] mobile:mt-7 tablet:mt-[53px] desktop:mt-0">
@@ -65,10 +98,10 @@ function DestinationContent() {
             })}
           </div>
           <h1 className="text-white uppercase font-bellefair mobile:text-[56px] tablet:text-[80px] desktop:text-[100px] mt-[10px] mb-[10px]">
-            {moon?.name}
+            {foundDestination?.name}
           </h1>
           <p className="text-babyBlue mobile:text-[15px] tablet:text-[16px] desktop:text-[18px] font-barlow mobile:leading-[25px] tablet:leading-[28px] desktop:leading-[32px]">
-            {moon?.description || ''}
+            {foundDestination?.description || ''}
           </p>
           <hr className="border-[#383B4B] mt-[54px] mb-[28px]" />
           <div className="grid grid-cols-2">
@@ -77,7 +110,7 @@ function DestinationContent() {
                 AVG. DISTANCE
               </h3>
               <p className="text-white text-[28px] font-bellefair">
-                {moon?.distance || ''}
+                {foundDestination?.distance || ''}
               </p>
             </div>
             <div className="">
@@ -85,7 +118,7 @@ function DestinationContent() {
                 AVG. DISTANCE
               </h3>
               <p className="text-white text-[28px] font-bellefair">
-                {moon?.travel}
+                {foundDestination?.travel}
               </p>
             </div>
           </div>
