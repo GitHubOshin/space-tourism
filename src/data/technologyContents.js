1d00f86aea989083a66b3954753e6f4e4b8bc44f
OSHIN <izkp44@gmail.com>
2023-06-11 20:43:57 +0700

    Update technologyContents.js: add /

--- src/data/technologyContents.js
+++ src/data/technologyContents.js
@@ -4,8 +4,8 @@ const technologyContents = [
     title: 'LAUNCH VEHICLE',
     about: `A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!`,
     images: {
-      landscape: 'images/technology/image-launch-vehicle-landscape.jpg',
-      portrait: 'images/technology/image-launch-vehicle-portrait.jpg'
+      landscape: '/images/technology/image-launch-vehicle-landscape.jpg',
+      portrait: '/images/technology/image-launch-vehicle-portrait.jpg'
     }
   },
   {
@@ -13,8 +13,8 @@ const technologyContents = [
     title: 'SPACEPORT',
     about: `A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.`,
     images: {
-      landscape: 'images/technology/image-spaceport-landscape.jpg',
-      portrait: 'images/technology/image-spaceport-portrait.jpg'
+      landscape: '/images/technology/image-spaceport-landscape.jpg',
+      portrait: '/images/technology/image-spaceport-portrait.jpg'
     }
   },
   {
@@ -22,8 +22,8 @@ const technologyContents = [
     title: 'SPACE CAPSULE',
     about: `A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.`,
     images: {
-      landscape: 'images/technology/image-space-capsule-landscape.jpg',
-      portrait: 'images/technology/image-space-capsule-portrait.jpg'
+      landscape: '/images/technology/image-space-capsule-landscape.jpg',
+      portrait: '/images/technology/image-space-capsule-portrait.jpg'
     }
   }
 ]
