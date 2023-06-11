6d0bc451ef6da59399dc74df9772ecf121004b91
OSHIN <izkp44@gmail.com>
2023-06-11 20:45:00 +0700

    Update DestinationPage.js: add /

--- src/pages/DestinationPage.js
+++ src/pages/DestinationPage.js
@@ -6,17 +6,17 @@ function DestinationPage() {
     <div className="bg-cyan-500">
       <img
         alt="background"
-        src="images/destination/background-destination-desktop.jpg"
+        src="/images/destination/background-destination-desktop.jpg"
         className="w-full h-full mobile:hidden desktop:block"
       />
       <img
         alt="background"
-        src="images/destination/background-destination-tablet.jpg"
+        src="/images/destination/background-destination-tablet.jpg"
         className="w-full h-full mobile:hidden tablet:block desktop:hidden min-w-[375px]"
       />
       <img
         alt="background"
-        src="images/destination/background-destination-mobile.jpg"
+        src="/images/destination/background-destination-mobile.jpg"
         className="w-full h-full tablet:hidden min-w-[375px]"
       />
       <div className="absolute top-0 w-full h-full flex flex-col items-center gap-y-[76px] pt-[24px] tablet:pt-0 px-[24px] tablet:px-0">
